f3fcf03c006af29074eaf43f68d646d1c4b1d2d8
Shahriyor <shahriyordeveloping@gmail.com>
2021-09-23 17:22:24 +0300

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -13,3 +13,17 @@ ripple.forEach(btn => {
     }, 500);
   })
 })
+
+
+const openDrop = (el) => {
+  el.classList.toggle('active');
+}
+
+window.addEventListener('click', (e) => {
+  if(!e.target.classList.contains('dropdown__button') && document.querySelector('.dropdown__button.active')){
+    const btns = document.querySelectorAll('.dropdown__button');
+    btns.forEach(btn => {
+      btn.classList.remove('active')
+    })
+  }
+})
