9b9d4c0434a6aa1c66b29e837bbb8a087187257a
Shahriyor <shahriyordeveloping@gmail.com>
2021-10-02 16:19:50 +0300

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -31,8 +31,6 @@ if(document.querySelector('.fixed-menu__button')){
 const ripple = document.querySelectorAll('.ripple')
 ripple.forEach(btn => {
   btn.addEventListener('click', function(e){
-    console.log('click');
-    
     let x = e.clientX - e.target.offsetLeft;
     let y = e.clientY - e.target.offsetTop;
     let ripples = document.createElement('span');
